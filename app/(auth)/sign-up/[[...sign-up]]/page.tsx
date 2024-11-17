3f43f626e75c3a967c8dc8de401b317de465a193
Iaa <102237135+Iaaaaaaa@users.noreply.github.com>
2024-11-17 16:42:48 +0800

    .

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import * as SignUp from "@clerk/elements/sign-up";
 import * as Clerk from "@clerk/elements/common";
@@ -24,6 +24,14 @@ const poppins = Poppins({
 });
 
 const SignUpPage: React.FC = () => {
+  // Manage role state
+  const [role, setRole] = useState("");
+
+  // Handle role selection
+  const handleRoleChange = (value: string) => {
+    setRole(value);
+  };
+
   return (
     <main className="flex flex-col justify-center w-full min-h-screen px-5 overflow-hidden bg-white sm:px-10">
       <div className="flex justify-center flex-grow mt-10">
@@ -84,9 +92,10 @@ const SignUpPage: React.FC = () => {
                     />
                   </Clerk.Field>
                 </div>
+
                 <Clerk.Field name="role">
                   <Clerk.Label className="sr-only">Role</Clerk.Label>
-                  <Select>
+                  <Select onValueChange={handleRoleChange}>
                     <SelectTrigger
                       className={`${poppins.className} w-full border-2 rounded-lg border-black bg-white pb-2 text-sm/6 outline-none placeholder:text-black`}
                     >
@@ -147,67 +156,6 @@ const SignUpPage: React.FC = () => {
                   </Clerk.Link>
                 </p>
               </div>
-            </SignUp.Step>
-            <SignUp.Step
-              name="verifications"
-              className="w-full px-4 py-10 space-y-6 bg-white shadow-md rounded-2xl ring-1 ring-black/5 sm:w-96 sm:px-8"
-            >
-              <header className="text-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 40 40"
-                  className="mx-auto size-10 text-zinc-950"
-                  aria-hidden
-                >
-                  <mask
-                    id="a"
-                    width="40"
-                    height="40"
-                    x="0"
-                    y="0"
-                    maskUnits="userSpaceOnUse"
-                  >
-                    <circle cx="20" cy="20" r="20" fill="#D9D9D9" />
-                  </mask>
-                  <g fill="currentColor" mask="url(#a)">
-                    <path d="M43.5 3a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46V2ZM43.5 8a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46V7ZM43.5 13a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 18a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 23a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 28a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 33a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 38a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1Z" />
-                    <path d="M27 3.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM25 8.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM23 13.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM21.5 18.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM20.5 23.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM22.5 28.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM25 33.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM27 38.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2Z" />
-                  </g>
-                </svg>
-                <h1 className="mt-4 text-xl font-medium tracking-tight text-zinc-950">
-                  Verify email code
-                </h1>
-              </header>
-              <Clerk.GlobalError className="block text-sm text-red-400" />
-              <SignUp.Strategy name="email_code">
-                <Clerk.Field name="code" className="space-y-2">
-                  <Clerk.Label className="text-sm font-medium text-zinc-950">
-                    Email code
-                  </Clerk.Label>
-                  <Clerk.Input
-                    type="otp"
-                    required
-                    className="w-full rounded-md bg-white px-3.5 py-2 text-sm outline-none ring-1 ring-inset ring-zinc-300 hover:ring-zinc-400 focus:ring-[1.5px] focus:ring-zinc-950 data-[invalid]:ring-red-400"
-                  />
-                  <Clerk.FieldError className="block text-sm text-red-400" />
-                </Clerk.Field>
-                <SignUp.Action
-                  submit
-                  className="w-full rounded-md bg-zinc-950 px-3.5 py-1.5 text-center text-sm font-medium text-white shadow outline-none ring-1 ring-inset ring-zinc-950 hover:bg-zinc-800 focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-zinc-950 active:text-white/70"
-                >
-                  Verify
-                </SignUp.Action>
-              </SignUp.Strategy>
-              <p className="text-sm text-center text-zinc-500">
-                Already have an account?{" "}
-                <Clerk.Link
-                  navigate="sign-in"
-                  className="font-medium outline-none text-zinc-950 decoration-zinc-950/20 underline-offset-4 hover:text-zinc-700 hover:underline focus-visible:underline"
-                >
-                  Sign in
-                </Clerk.Link>
-              </p>
             </SignUp.Step>
           </SignUp.Root>
         </div>
